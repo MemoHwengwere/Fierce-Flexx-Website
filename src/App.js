274aec3f85c9ad9046cf07c3092e7024b6721828
Memo Hwengwere <122862820+MemoHwengwere@users.noreply.github.com>
2025-04-11 21:31:26 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Fierce Flexx!</h1>
+      <p>Your bold and empowering plus-size fashion brand.</p>
+    </div>
+  );
+}
+
+export default App;
